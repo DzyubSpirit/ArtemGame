9071096dcdcf2c959251b4850f4387107a0a94fa
DzyubSpirit <Dzyubavlad@gmail.com>
2015-03-08 11:11:39 +0200

    All to this branch

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.onload = function() {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
 	
+	var timer = setInterval(paint, 33);
+	
+	function paint() {
+		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+	}
 }
+
+
+
